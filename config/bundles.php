34f4b50ad04710ea44fece390a9bb7261a920651
derekmcc <derek5mccarthy@gmail.com>
2018-04-15 21:34:02 +0100

    Testing

--- config/bundles.php
+++ config/bundles.php
@@ -10,7 +10,7 @@ return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
    // Koff\Bundle\CrudMakerBundle\CrudMakerBundle::class => ['all' => true],
-   // Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
